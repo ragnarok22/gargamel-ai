c22dd3ee36839c5d1324404d567921d074eb8d59
Reinier Hernández <sasuke.reinier@gmail.com>
2026-04-21 10:00:28 -0300

    refactor(main): wrap loop in main entrypoint with keyboard interrupt handling

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 pir = Pin(27, Pin.IN)
 
 
-while True:
-    print(pir.value())
-    if pir.value():
-        scary.animate(oled)
-        winky.animate(oled)
-    else:
-        neutral.animate(oled)
-        time.sleep_ms(200)
+def main():
+    while True:
+        print(pir.value())
+        if pir.value():
+            scary.animate(oled)
+            winky.animate(oled)
+        else:
+            neutral.animate(oled)
+            time.sleep_ms(200)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
